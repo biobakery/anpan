1b7decf69e49b8cfbc7811fbf1601da73521abbf
Andrew Ghazi <andghazi@gmail.com>
2022-01-10 12:23:21 -0500

    Use the right function

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach = function(libname, pkgname) {
-  message("anpan uses the furrr and progressr packages.\n * Run future::plan() (before calling anpan) in a way that's appropriate for your system to parallelize.\n * Run library(progressr); handlers(global=TRUE) to show progress.")
+  packageStartupMessage("anpan uses the furrr and progressr packages.\n * Run future::plan() (before calling anpan) in a way that's appropriate for your system to parallelize.\n * Run library(progressr); handlers(global=TRUE) to show progress.")
 }
