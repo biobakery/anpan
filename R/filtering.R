4dab4f7c830e40dc00b406e15ebb24e1a44e7b26
Andrew Ghazi <andghazi@gmail.com>
2022-02-03 10:36:13 -0500

    Switch to using nonzero observations to fix large bug behavior

--- R/filtering.R
+++ R/filtering.R
@@ -75,101 +75,15 @@ get_samp_stats = function(gf){
     .[]
 }
 
-fit_mixture = function(samp_stats) {
-  em_input = na.omit(samp_stats[,.(sample_id, n_z, q50)])
-  em_input$n_z = scale(em_input$n_z)
-  em_input$q50 = scale(em_input$q50)
-
-  mf = mixtools::mvnormalmixEM(as.matrix(em_input[,-1]),
-                               lambda = c(.75, .25),
-                               mu = list(c(-1, 1), 1, -1))
-  return(list(res = mf, input = em_input))
-}
-
-get_component_densities = function(mix_fit, plot = FALSE) {
-  input_mat = as.matrix(mix_fit$input[,-1])
-
-  left_i = which.min(sapply(mix_fit$res$mu, function(.x) .x[1]))
-  right_i = setdiff(1:2, left_i)
-
-  samp_df = mix_fit$input
-  samp_df$left_dens = mixtools::dmvnorm(input_mat,
-                                        mu = mix_fit$res$mu[[left_i]],
-                                        sigma = mix_fit$res$sigma[[left_i]])
-  samp_df$right_dens = mixtools::dmvnorm(input_mat,
-                                        mu = mix_fit$res$mu[[right_i]],
-                                        sigma = mix_fit$res$sigma[[right_i]])
-
-  samp_df[,in_right := right_dens > left_dens]
-
-  if (plot) {
-    samp_df %>%
-      ggplot(aes(n_z, q50)) +
-      geom_point(aes(color = in_right)) +
-      labs(title = 'EM based decision for A. putredinis') +
-      theme_light()
-  }
-
-  samp_df[,.(sample_id, in_right, left_dens, right_dens)]
-}
-
-# TODO implement this
-# Check if the components overlap mostly overlap
-# Turns out this is pretty hard to do:
-# https://math.stackexchange.com/questions/1114879/detect-if-two-ellipses-intersect
-check_mix_fit = function(mix_fit) {
-  # Check they don't overlap too much
-  # Check that one is to the upper left of the other
-  TRUE
-}
-
-filter_with_mixture = function(gf,
-                               samp_stats,
-                               save_filter_stats,
-                               filter_stats_dir,
-                               bug_name){
-  mix_fit = fit_mixture(samp_stats)
-
-  mix_checks = check_mix_fit(mix_fit)
-
-  if (!mix_checks) {
-    stop("Mixture fitting seems to have failed")
-  }
-
-  if (save_filter_stats) {
-
-    make_hex_plot(samp_stats = samp_stats,
-                  mix_fit = mix_fit,
-                  plot_dir = file.path(filter_stats_dir, "plots"),
-                  bug_name = bug_name)
-
-    mix_fit_res = mix_fit$res
-
-    save(samp_stats, mix_fit_res,
-         file = file.path(filter_stats_dir, paste0(bug_name, ".RData")))
-  }
-
-  mix_labels = get_component_densities(mix_fit)
-  label_df = mix_labels[samp_stats, on = "sample_id"] # TODO write out this table
-  label_df$in_right[is.na(label_df$in_right)] = TRUE # all zero samples don't have the species
-
-  filtered_gf = label_df[,.(sample_id, in_right)][gf, on = "sample_id"]
-
-  filtered_gf$abd[filtered_gf$in_right] = 0
-  filtered_gf$present = filtered_gf$abd > 0
-
-  filtered_gf
-}
-
 filter_with_kmeans = function(gf,
                               samp_stats,
                               save_filter_stats,
                               filter_stats_dir,
                               bug_name) {
 
-  em_input = na.omit(samp_stats[,.(sample_id, n_z, q50)])
-  if (dplyr::n_distinct(gf$gene) > 50000) em_input$n_z = log1p(em_input$n_z)
-  em_input$n_z = scale(em_input$n_z)
+  em_input = na.omit(samp_stats[,.(sample_id, n_nz, q50)])
+  if (dplyr::n_distinct(gf$gene) > 50000) em_input$n_nz = log1p(em_input$n_nz)
+  em_input$n_nz = scale(em_input$n_nz)
 
   scrunch = 2 # Scrunch the y-axis of the plots to make sure k-means doesn't accidentally produce a horizontal decision boundary. (i.e cut off the top of the U shape)
   # TODO make sure this doesn't mess up anything downstream
@@ -195,7 +109,7 @@ filter_with_kmeans = function(gf,
   }
 
   filtered_gf = samp_stats[,.(sample_id, in_right)][gf, on = "sample_id"]
-  filtered_gf$abd[filtered_gf$in_right] = 0
+  filtered_gf$abd[!filtered_gf$in_right] = 0
   filtered_gf$present = filtered_gf$abd > 0
 
   filtered_gf
@@ -212,15 +126,9 @@ filter_gf = function(gf,
                      filter_stats_dir = NULL,
                      bug_name = NULL) {
 
-  if (!(filtering_method %in% c("med_by_nz_components", "kmeans", "none"))) stop("Specified filtering method not implemented")
+  if (!(filtering_method %in% c("kmeans", "none"))) stop("Specified filtering method not implemented")
 
-  if (filtering_method == "med_by_nz_components"){
-    filtered_gf = filter_with_mixture(gf,
-                                      samp_stats = samp_stats,
-                                      save_filter_stats = save_filter_stats,
-                                      filter_stats_dir = filter_stats_dir,
-                                      bug_name = bug_name)
-  } else if (filtering_method == 'kmeans') {
+  if (filtering_method == 'kmeans') {
     filtered_gf = filter_with_kmeans(gf = gf,
                                      samp_stats = samp_stats,
                                      save_filter_stats = save_filter_stats,
@@ -338,7 +246,7 @@ read_and_filter = function(bug_file, metadata, # TODO make metadata optional for
                           bug_name = bug_name) # Might need to reapply the minmax_thresh here
 
   if (filtering_method != "none") { # TODO separate these four blocks out to a distinct function
-    sample_labels = unique(filtered_gf[,.(sample_id, bug_present = !in_right)])
+    sample_labels = unique(filtered_gf[,.(sample_id, bug_present = in_right)])
     n_absent = sum(!sample_labels$bug_present)
   }
 
@@ -352,7 +260,7 @@ read_and_filter = function(bug_file, metadata, # TODO make metadata optional for
   }
 
   if (discard_absent_samples && filtering_method != "none") {
-    filtered_gf = filtered_gf[!(in_right)]
+    filtered_gf = filtered_gf[(in_right)]
     if (n_absent != 0 & verbose) message("* Samples with no ", bug_name, " discarded.")
   }
 
--- R/visualization.R
+++ R/visualization.R
@@ -1,24 +1,3 @@
-get_ellipse = function(mu, sigma) {
-  circ_df = tibble(t = seq(0, 2*pi, by = .01),
-                   x = cos(t),
-                   y = sin(t))
-  circ = as.matrix(circ_df[,c('x', 'y')])
-
-  m = (circ %*% sigma) %*% matrix(c(qnorm(.975), 0, 0, qnorm(.975)), nrow = 2)
-  m[,1] = m[,1] + mu[1]
-  m[,2] = m[,2] + mu[2]
-  # as_tibble(m)
-  t(m)
-}
-
-get_transformed_ellipse = function(id, mix_fit, A, input_scales){
-  mat = A %*% get_ellipse(mix_fit$res$mu[[id]], mix_fit$res$sigma[[id]])
-  mat = t(mat)
-  mat[,1] = mat[,1] + input_scales$m_n_z
-  mat[,2] = mat[,2] + input_scales$m_med
-  as_tibble(mat) %>% purrr::set_names(c('n_z', 'q50'))
-}
-
 #' Make a filter-labelled line plot
 #' @param bug_file a path to a gene family file
 #' @param meta_file a path to the corresponding metadata file
@@ -50,7 +29,7 @@ make_line_plot = function(bug_file = NULL,
   }
 
   p = fgf[is.finite(labd)][order(-labd)][, i := 1:(nrow(.SD)), by = sample_id][] %>%
-    dplyr::mutate(labelled_as = factor(c('present', 'absent')[in_right + 1],
+    dplyr::mutate(labelled_as = factor(c('absent', 'present')[in_right + 1],
                                        levels = c("present", "absent"))) %>%
     ggplot(aes(i, labd)) +
     geom_line(aes(group = sample_id,
@@ -76,7 +55,7 @@ make_kmeans_dotplot = function(samp_stats,
                                bug_name = NULL) {
 
   p = samp_stats %>%
-    dplyr::mutate(labelled_as = factor(c('present', 'absent')[in_right + 1],
+    dplyr::mutate(labelled_as = factor(c('absent', 'present')[in_right + 1],
                                        levels = c("present", "absent"))) %>%
     ggplot(aes(n_z, q50)) +
     geom_point(aes(color = labelled_as),
@@ -90,84 +69,7 @@ make_kmeans_dotplot = function(samp_stats,
 
   if (!is.null(plot_dir)) {
     ggsave(p,
-           filename = file.path(plot_dir, paste0(bug_name, "_hex.png")),
-           width = 6, height = 4)
-  }
-
-  p
-}
-
-#' Make a hex plot of the abundance by zeroness of a gene family dataset
-#' @param bug_file path to a gene family file
-#' @param meta_file path to a metadata file
-#' @param samp_stats data frame of sample statistics
-#' @param mix_fit mixture fit object
-#' @details The required input is either \itemize{ \item{}{the path to the gene
-#'   family file and the metadata file} \item{}{OR a set of sample statistics
-#'   and a mixture fit} } The first option is simpler but slower and (very
-#'   slightly) stochastic. Defaults to using samp_stats and mix_fit if all three
-#'   are supplied (but don't do that anyway).
-#' @param bug_name the name of the bug
-#' @export
-make_hex_plot = function(bug_file = NULL,
-                         meta_file = NULL,
-                         minmax_thresh = 5,
-                         samp_stats = NULL,
-                         mix_fit = NULL,
-                         bug_name = NULL,
-                         plot_dir = NULL) {
-
-  precomputed = (!is.null(samp_stats) & !is.null(mix_fit))
-  to_compute = (!is.null(bug_file) & !is.null(meta_file))
-
-  if (to_compute) {
-    gf = read_bug(bug_file, meta = read_meta(meta_file))[,.(gene, sample_id, abd, varies_enough = sum(abd != 0) < (.N - minmax_thresh) & sum(abd != 0) > minmax_thresh), by = gene
-    ][(varies_enough)
-    ][,.(gene, sample_id, abd)]
-
-    samp_stats = get_samp_stats(gf)
-
-    mix_fit = fit_mixture(samp_stats)
-  } else if (precomputed) {
-    # Do nothing
-  } else {
-    stop("You somehow misspecified your inputs. Specify either a gene family file and metadata file OR a sample statistics data frame and a mixture fit object")
-  }
-
-  em_input = na.omit(samp_stats[,.(sample_id, n_z, q50)])
-  em_input$sn_z = scale(em_input$n_z)
-  em_input$sq50 = scale(em_input$q50)
-  input_scales = em_input[,.(m_n_z = mean(n_z),
-                             m_med = mean(q50),
-                             s_n_z = sd(n_z),
-                             s_med = sd(q50))]
-  em_input[,`:=`(centered_n_z = n_z - input_scales$m_n_z,
-                 centered_med = q50 - input_scales$m_med)]
-
-  s = t(as.matrix(em_input[1:2, .(sn_z, sq50)]))
-  C = t(as.matrix(em_input[1:2, .(centered_n_z, centered_med)]))
-  s_inv = solve(s)
-  A = C %*% s_inv # This is the transformation between standardized and centered scales
-
-  p = em_input %>%
-    ggplot(aes(n_z, q50)) +
-    geom_hex(aes(color = ..count..),
-             lwd = .1) +
-    scale_fill_viridis_c() +
-    scale_color_viridis_c() +
-    geom_path(color = 'red',
-              data = get_transformed_ellipse(id = 1, mix_fit, A, input_scales)) +
-    geom_path(color = 'red',
-              data = get_transformed_ellipse(id = 2, mix_fit, A, input_scales)) +
-    guides(color = guide_none()) +
-    labs(title = paste0(bug_name, " - 2 component mixture of median by n_zero observations"),
-         x = "Number of zero observations",
-         y = 'Median log abundance') +
-    theme_light()
-
-  if (!is.null(plot_dir)) {
-    ggsave(p,
-           filename = file.path(plot_dir, paste0(bug_name, "_hex.png")),
+           filename = file.path(plot_dir, paste0(bug_name, "_kmeans.png")),
            width = 6, height = 4)
   }
 
@@ -452,7 +354,6 @@ make_composite_plot = function(bug_file,
   lp = make_line_plot(bug_file,
                       covariates = covariates,
                       outcome = outcome) #
-  hp = make_hex_plot(bug_file)
 
   dp = make_data_plot(bug_file,
                       covariates = covariates,
