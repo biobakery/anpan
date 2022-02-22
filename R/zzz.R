263ed3cb06691c1411a4d91aef166effea5f1433
Andrew Ghazi <andghazi@gmail.com>
2022-02-22 10:14:54 -0500

    better startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach = function(libname, pkgname) {
-  packageStartupMessage("* Get help: Visit the biobakery help forum at https://forum.biobakery.org/\n* Parallelize: Before calling anpan, run future::plan() in a way that's appropriate for your system.\n* Show progress: Run library(progressr); handlers(global=TRUE)")
+  packageStartupMessage("* Get help: Visit the biobakery help forum at https://forum.biobakery.org/\n* Parallelize: Before calling anpan, run future::plan() in a way that's appropriate for your system.\n* Show progress: Before calling anpan, run library(progressr); handlers(global=TRUE)")
 }
