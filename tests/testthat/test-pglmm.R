bbb326ae5be75a8c52ba06c895666a17659dfa57
Andrew Ghazi <andghazi@gmail.com>
2022-08-23 12:00:06 -0400

    binary outcome test

--- tests/testthat/test-pglmm.R
+++ tests/testthat/test-pglmm.R
@@ -8,5 +8,18 @@ test_that("pglmm runs", {
                     show_plot_cor_mat = FALSE,
                     show_plot_tree = FALSE)
 
-  expect_is(res, "list")
+  expect_type(res, "list")
+})
+
+test_that("logistic pglmm runs", {
+  meta = data.frame(x = as.logical(rbinom(100, 1, prob = .5)), sample_id = paste0("t", 1:100))
+  tr = ape::rtree(100)
+  res = anpan_pglmm(meta, tr,
+                    outcome = "x",
+                    iter_sampling = 10,
+                    iter_warmup = 10,
+                    show_plot_cor_mat = FALSE,
+                    show_plot_tree = FALSE)
+
+  expect_type(res, "list")
 })
