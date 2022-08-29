bda44d48c15905b965eb68310ad02004b98bec4a
Andrew Ghazi <andghazi@gmail.com>
2022-08-29 13:50:50 -0400

    remove comments

--- R/read_in_data.R
+++ R/read_in_data.R
@@ -60,18 +60,7 @@ get_file_list = function(file_dir) {
   list.files(file_dir, full.names = TRUE)
 }
 
-# The readr progress messes up the progressr bar
 write_tsv_no_progress = function(x, file, append = FALSE) {
-
-  # old = options("readr.show_progress")$readr.show_progress
-  #
-  # options(readr.show_progress = FALSE)
-  #
-  # readr::write_tsv(x = x,
-  #                  file = file,
-  #                  append = append)
-  #
-  # options(readr.show_progress = old)
 
   data.table::fwrite(x = x,
                      file = file,
