ca64e8c18d6c7ae4582500943a1788b60427dc52
Andrew Ghazi <andghazi@gmail.com>
2022-09-27 23:42:08 -0400

    make the offset threshold lower -_-

--- R/pglmm_loo.R
+++ R/pglmm_loo.R
@@ -314,7 +314,7 @@ log_lik_i_j_logistic = function(j, lm_mean, sigma12x22_inv, sigma21,
       int_res$result$value == 0 ||
       int_res$result$value < 1e-4 ||
       int_res$result$value > 100 ||
-      abs(offset_j) > 100) {
+      abs(offset_j) > 25) {
     opt_res = safely_optim(par = mu_bar_j,
                            fn = vec_integrand_logistic,
                            method = "L-BFGS-B",
