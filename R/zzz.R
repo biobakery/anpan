a51a8138faf4f062ad0898d1e8d0a2b84887824d
Andrew Ghazi <andghazi@gmail.com>
2023-06-29 13:18:53 -0400

    Add vignette to startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,20 @@
 .onAttach = function(libname, pkgname) {
   ver = utils::packageVersion("anpan")
+
+  vignette_built = nrow(vignette(package = 'anpan')$results) != 0
+
+  if (vignette_built) {
+    rebuild_advice = ""
+  } else {
+    rebuild_advice = "Reinstall with build_vignettes = TRUE, then "
+  }
+
+  vignette_msg = paste0('- Read the vignette: ',
+                        rebuild_advice,
+                        'browseVignettes("anpan")')
+
   packageStartupMessage("This is anpan version ", ver)
+  packageStartupMessage(vignette_msg)
   packageStartupMessage("- Get help: Visit the biobakery help forum at https://forum.biobakery.org/")
   packageStartupMessage("- Parallelize: Before calling anpan, run future::plan() in a way that's appropriate for your system.")
   packageStartupMessage("- Show progress: Before calling anpan, run library(progressr); handlers(global=TRUE)")
