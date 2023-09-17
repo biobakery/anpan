b3f7a3d73910a37977f4856c1d6c8c7d73f7ad3b
Andrew Ghazi <andghazi@gmail.com>
2023-09-17 17:00:06 -0500

    fix imports, suggests

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,7 @@
 #' @import dplyr ggplot2 patchwork purrr
 #' @rawNamespace import(data.table, except = c(first, last, between, transpose))
+#' @importFrom stats as.formula complete.cases cor dbinom dist dnorm effects hclust kmeans model.matrix na.omit p.adjust qnorm quantile sd
+#' @importFrom utils capture.output combn head vignette
+#' @importFrom future plan
+#' @importFrom methods is
 NULL
