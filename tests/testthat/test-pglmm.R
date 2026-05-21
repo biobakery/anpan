6fc7a02f4dac79dd67a0f59a7d8bf6cbdfece8f6
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2026-05-21 15:29:04 -0400

    documentation update, tests for gaussian loglik calculation + test data

--- tests/testthat/test-pglmm.R
+++ tests/testthat/test-pglmm.R
@@ -1,12 +1,17 @@
 test_that("pglmm runs", {
   meta = data.frame(x = rnorm(100), sample_id = paste0("t", 1:100))
   tr = ape::rtree(100)
-  res = anpan_pglmm(meta, tr,
+
+  # There will be warnings because the chains are so short.
+
+  # This is mainly to force the models to compile during tests. This is done so
+  # root can compile the models on the Guacamole VMs.
+  suppressWarnings({res = anpan_pglmm(meta, tr,
                     outcome = "x",
                     iter_sampling = 10,
                     iter_warmup = 10,
                     show_plot_cor_mat = FALSE,
-                    show_plot_tree = FALSE)
+                    show_plot_tree = FALSE)})
 
   expect_type(res, "list")
 })
@@ -14,13 +19,15 @@ test_that("pglmm runs", {
 test_that("logistic pglmm runs", {
   meta = data.frame(x = as.logical(rbinom(100, 1, prob = .5)), sample_id = paste0("t", 1:100))
   tr = ape::rtree(100)
-  res = anpan_pglmm(meta, tr,
+
+  # There will be warnings because the chains are so short
+  suppressWarnings({res = anpan_pglmm(meta, tr,
                     family = "binomial",
                     outcome = "x",
                     iter_sampling = 10,
                     iter_warmup = 10,
                     show_plot_cor_mat = FALSE,
-                    show_plot_tree = FALSE)
+                    show_plot_tree = FALSE)})
 
   expect_type(res, "list")
 })
