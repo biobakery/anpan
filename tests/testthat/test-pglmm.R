ffa9b18accd3412403d44c090384de2a6a41f992
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2026-05-30 13:48:28 -0400

    tests for binomial ll calculations, edits to functions that run Stan to make them stfu

--- tests/testthat/test-pglmm.R
+++ tests/testthat/test-pglmm.R
@@ -6,13 +6,23 @@ test_that("pglmm runs", {
 
   # This is mainly to force the models to compile during tests. This is done so
   # root can compile the models on the Guacamole VMs.
-  suppressWarnings({res = anpan_pglmm(meta, tr,
+  opt = options("cmdstanr_warn_inits")[[1]]
+
+  options(cmdstanr_warn_inits = FALSE)
+  suppressMessages(suppressWarnings({res = anpan_pglmm(meta, tr,
                     outcome = "x",
                     iter_sampling = 10,
                     iter_warmup = 10,
                     show_plot_cor_mat = FALSE,
-                    show_plot_tree = FALSE)})
+                    show_plot_tree = FALSE,
+                    show_post = FALSE,
+                    show_messages = FALSE,
+                    show_exceptions = FALSE,
+                    verbose = FALSE,
+                    run_diagnostics = FALSE,
+                    )}))
 
+  options("cmdstanr_warn_inits" = opt)
   expect_type(res, "list")
 })
 
@@ -20,14 +30,24 @@ test_that("logistic pglmm runs", {
   meta = data.frame(x = as.logical(rbinom(100, 1, prob = .5)), sample_id = paste0("t", 1:100))
   tr = ape::rtree(100)
 
+  opt = options("cmdstanr_warn_inits")[[1]]
+  options(cmdstanr_warn_inits = FALSE)
+
   # There will be warnings because the chains are so short
-  suppressWarnings({res = anpan_pglmm(meta, tr,
+  suppressMessages(suppressWarnings({res = anpan_pglmm(meta, tr,
                     family = "binomial",
                     outcome = "x",
                     iter_sampling = 10,
                     iter_warmup = 10,
                     show_plot_cor_mat = FALSE,
-                    show_plot_tree = FALSE)})
+                    show_plot_tree = FALSE,
+                    show_post = FALSE,
+                    show_messages = FALSE,
+                    show_exceptions = FALSE,
+                    verbose = FALSE,
+                    run_diagnostics = FALSE,
+                    )}))
 
+  options("cmdstanr_warn_inits" = opt)
   expect_type(res, "list")
 })
