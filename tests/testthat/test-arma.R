75bf0003307ab29b4ce1c38beee18b0122db1c9f
Andrew Ghazi <6763470+andrewGhazi@users.noreply.github.com>
2026-07-22 15:12:02 -0400

    add old woodbury implementation to test file

--- tests/testthat/test-arma.R
+++ tests/testthat/test-arma.R
@@ -1,3 +1,25 @@
+woodbury_s22_inv_old = function(cor_mat_inv, cor_mat, j) {
+  # Redone with Rcpp, see binom_ll.cpp
+
+  # https://en.wikipedia.org/wiki/Woodbury_matrix_identity
+  # >20x faster, but less numerically precise by a tiny amount.
+
+  n = nrow(cor_mat)
+  ord = c(j, (1:n)[-j])
+  rcor_mat = cor_mat[ord,ord]
+  rcor_mat_inv = cor_mat_inv[ord,ord]
+
+  U = cbind(c(1, rep(0, n-1)),
+            c(0, rcor_mat[-1, 1]))
+
+  V = t(U)[2:1,]
+
+  update_factor = solve(diag(2) - V %*% (rcor_mat_inv %*% U))
+
+  woodbury_ans = rcor_mat_inv + (rcor_mat_inv %*% U) %*% update_factor %*% (V %*% rcor_mat_inv)
+
+  woodbury_ans[-1,-1]
+}
 
 test_that("arma::solve matches base", {
   X = matrix(rnorm(100), ncol = 10)
